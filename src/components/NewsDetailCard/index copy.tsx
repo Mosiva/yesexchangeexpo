d188a436839735d2577710d9c8e18352d5e276f8
sultabsk <sultabsk@gmail.com>
2025-11-28 17:31:11 +0500

    modify news cart

---
+++ src/components/NewsDetailCard/index copy.tsx
@@ -0,0 +1,192 @@
+import dayjs from "dayjs";
+import { useRouter } from "expo-router";
+import React, { memo } from "react";
+import {
+  Image,
+  Linking,
+  Platform,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import Markdown from "react-native-markdown-display";
+import { useTheme } from "../../hooks/useTheme";
+
+type Props = {
+  title: string;
+  date: string;
+  content: string;
+  image?: string;
+  source: string;
+  url: string;
+};
+
+export default function NewsDetailCard({
+  title,
+  date,
+  content,
+  image,
+  source,
+  url,
+}: Props) {
+  const router = useRouter();
+  console.log(url);
+  const { colors } = useTheme();
+
+  const styles = makeStyles(colors);
+  return (
+    <ScrollView style={styles.container} bounces>
+      {/* Cover image / placeholder */}
+      <View style={styles.coverWrap}>
+        {typeof image === "string" ? (
+          <Image source={{ uri: image }} style={styles.cover} />
+        ) : (
+          <View style={[styles.cover, styles.coverFallback]} />
+        )}
+
+        {/* Back button */}
+        {/* <Pressable
+          onPress={() => router.back()}
+          hitSlop={10}
+          style={styles.backBtn}
+        >
+          <Ionicons name="arrow-back" size={20} color="#111827" />
+        </Pressable> */}
+      </View>
+
+      {/* Article */}
+      <View style={styles.body}>
+        <Text style={[styles.date, { color: colors.subtext }]}>{source}</Text>
+        <Text style={[styles.title, { color: colors.text }]}>{title}</Text>
+        <Text style={[styles.date, { color: colors.subtext }]}>
+          {dayjs(date).format("DD.MM.YYYY")}
+        </Text>
+
+        <ArticleText text={content} />
+        <Pressable onPress={() => Linking.openURL(url)} style={styles.button}>
+          <Text style={styles.buttonText}>Источник</Text>
+        </Pressable>
+      </View>
+    </ScrollView>
+  );
+}
+
+const ArticleText = memo(({ text }: { text: string }) => {
+  const { colors } = useTheme();
+  const styles = makeStyles(colors);
+
+  return (
+    <View style={{ marginTop: 10 }}>
+      <Markdown style={markdownStyles(colors)}>{text}</Markdown>
+    </View>
+  );
+});
+ArticleText.displayName = "ArticleText";
+
+const COVER_H = 220;
+
+const makeStyles = (colors: any) =>
+  StyleSheet.create({
+    container: {
+      backgroundColor: colors.background,
+      flex: 1,
+    },
+    button: {
+      marginTop: 16,
+      backgroundColor: colors.primary,
+      paddingVertical: 12,
+      borderRadius: 8,
+      alignItems: "center",
+    },
+    buttonText: {
+      color: colors.text,
+      fontSize: 16,
+      fontWeight: "600",
+    },
+    /* Cover */
+    coverWrap: {
+      height: COVER_H,
+      position: "relative",
+      overflow: "hidden",
+      backgroundColor: colors.card,
+    },
+    cover: {
+      width: "100%",
+      height: "100%",
+      resizeMode: "cover",
+    },
+    coverFallback: {
+      backgroundColor: colors.card,
+    },
+    backBtn: {
+      position: "absolute",
+      left: 12,
+      top: Platform.select({ ios: 50, android: 50 }),
+      width: 36,
+      height: 36,
+      borderRadius: 10,
+      backgroundColor: "rgba(255,255,255,0.9)",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+
+    /* Content */
+    body: {
+      paddingHorizontal: 20,
+      paddingTop: 16,
+      paddingBottom: 28,
+    },
+    title: {
+      fontSize: 20,
+      lineHeight: 26,
+      fontWeight: "800",
+      color: "#111827",
+      marginBottom: 6,
+    },
+    date: {
+      fontSize: 13,
+      color: "#6B7280",
+      marginBottom: 8,
+    },
+    p: {
+      fontSize: 15,
+      lineHeight: 22,
+      color: colors.text,
+    },
+  });
+const markdownStyles = (colors: any) => ({
+  body: {
+    color: colors.text,
+    fontSize: 15,
+    lineHeight: 22,
+  },
+  heading1: {
+    fontSize: 24,
+    fontWeight: "700",
+    marginTop: 16,
+    marginBottom: 8,
+    color: colors.text,
+  },
+  heading2: {
+    fontSize: 20,
+    fontWeight: "700",
+    marginTop: 14,
+    marginBottom: 6,
+    color: colors.text,
+  },
+  paragraph: {
+    marginTop: 12,
+    marginBottom: 12,
+  },
+  list_item: {
+    marginVertical: 4,
+  },
+  bullet_list: {
+    marginVertical: 6,
+  },
+  link: {
+    color: colors.primary,
+  },
+});
