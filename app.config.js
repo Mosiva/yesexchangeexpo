625e86d1e5defd883412768ccc3ebfc55ac7bcdb
sultabsk <sultabsk@gmail.com>
2025-12-30 10:54:02 +0500

    feat add about company

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Yesexchange",
     slug: "yesexchangeexpo",
-    version: "1.0.1",
+    version: "1.0.2",
     orientation: "portrait",
     icon: "./assets/images/iconorange.png",
     scheme: "yesexchangeexpo",
@@ -11,7 +11,7 @@ export default {
 
     ios: {
       supportsTablet: true,
-      buildNumber: "25",
+      buildNumber: "27",
       bundleIdentifier: "com.mosiva.yesexchangeexpo",
       infoPlist: {
         NSLocationWhenInUseUsageDescription:
@@ -23,7 +23,7 @@ export default {
 
     android: {
       package: "com.mosiva.yesexchangeexpo",
-      versionCode: 25,
+      versionCode: 27,
       googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
       adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
